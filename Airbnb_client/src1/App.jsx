77c9f17baef41bf1eb3c4b3ad9154eb2ed020bf7
KD3_Aditi_83533 <aditijoshi418@gmail.com>
2024-05-27 23:47:26 +0530

    airbnb

---
+++ Airbnb_client/src1/App.jsx
@@ -0,0 +1,25 @@
+
+import { Route, Routes } from 'react-router-dom'
+import Login from './screens/Login'
+import Register from './screens/Register'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+  
+
+function App() {
+  return (
+  
+    <div className = 'container'>
+     <Routes>
+       <Route path='' element={<Login/>}/>
+       <Route path='login' element={<Login/>}/>
+       <Route path='register' element={<Register/>}/>
+
+     </Routes>
+     <ToastContainer />
+    </div>
+    
+
+  )
+}
+export default App
