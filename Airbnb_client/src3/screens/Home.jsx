4e8f01c3726ea9bac5841d1ed1f83246cdc8ca75
KD3_Aditi_83533 <aditijoshi418@gmail.com>
2024-05-28 23:23:45 +0530

    airbnb updated

---
+++ Airbnb_client/src3/screens/Home.jsx
@@ -0,0 +1,27 @@
+import Navbar from "../components/navbar"
+import DashboardItem from '../components/dashboardItem'
+
+function Home(){
+    return (
+        <div>
+      <Navbar />
+      <h2 className='page-header'>Home</h2>
+      <div className='row mt-5'>
+        <div className='col'>
+          <DashboardItem title='Users' value='100K' />
+        </div>
+        <div className='col'>
+          <DashboardItem title='Properties' value='100K' />
+        </div>
+        <div className='col'>
+          <DashboardItem title='Bookings' value='10K' />
+        </div>
+        <div className='col'>
+          <DashboardItem title='Revenue' value='$100b' />
+        </div>
+      </div>
+    </div>
+    )
+}
+
+export default Home
